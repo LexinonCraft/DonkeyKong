1f4335a6357147cf022ba7a5ebae4a811df12907
LexinonCraft <willi4wuff@gmail.com>
2026-08-25 16:31:28 +0200

    fix: limit dt

--- src/Constants.hpp
+++ src/Constants.hpp
@@ -127,6 +127,8 @@ constexpr float DONKEY_KONG_AFTER_FALL_FRAME_INTERVAL = 0.15f; // seconds betwee
 constexpr float BEAM_WIDTH = 3.f; // px, width of the beam entity
 
 constexpr const char *FALLBACK_CONTRIBUTORS_TEXT = "By Muvels and Lexinon";
+
+constexpr float MAX_DT = 0.05f; // seconds, maximum delta time to prevent large jumps in game state due to lag or pauses
 } // namespace constants
 
 #endif
