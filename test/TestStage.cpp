ae3391bdc2e4519b22972da4699527e8d0ac4b80
LexinonCraft <willi4wuff@gmail.com>
2026-08-26 17:07:26 +0200

    test: player update test

---
+++ test/TestStage.cpp
@@ -0,0 +1,11 @@
+#include "TestStage.hpp"
+
+#include "DK/model/entities/Player.hpp"
+
+TestStage::TestStage(int rng(), PlayerData &player_data) : Stage(rng, player_data) {
+    girder = entities.add_girder({100, -100}, {500, -300});
+    barrel = entities.add_barrel({400, -400});
+    player->enter_platform(girder, 200);
+}
+
+void TestStage::place_hammer() { auto hammer = entities.add_hammer_power_up(player->get_position() + sf::Vector2f{0, -20}); }
