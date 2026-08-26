92cd7da032f2ee04cbeb1848044a73a8378a9661
LexinonCraft <willi4wuff@gmail.com>
2026-08-26 15:46:09 +0200

    fix: formatting

--- src/model/entities/Beam.cpp
+++ src/model/entities/Beam.cpp
@@ -4,7 +4,7 @@
 #include "DK/model/util/EntityVisitor.hpp"
 
 Beam::Beam(Ref ref, std::shared_ptr<Platform> lower_platform, std::shared_ptr<Platform> upper_platform, float x_position)
-    : BaseEntity(ref), x(x_position), lower_y(lower_platform->surface_y_at(x_position)),
-      upper_y(upper_platform->surface_y_at(x_position)) {}
+    : BaseEntity(ref), x(x_position), lower_y(lower_platform->surface_y_at(x_position)), upper_y(upper_platform->surface_y_at(x_position)) {
+}
 
 void Beam::accept(EntityVisitor &visitor) { visitor.visit(*this); }
